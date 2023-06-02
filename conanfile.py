7ad3127cc4d3a9f1c7f406a951cb0e1c17fb6566
ShuangLiu <liushuang.work@gmail.com>
2023-06-02 13:20:42 +0100

    add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conan import ConanFile
+import conan.tools.files
+from conan.tools.cmake import CMake, CMakeToolchain
+
+
+class IMGUIConan(ConanFile):
+    name = "imgui"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "ImDrawIdx": ["unsigned int", "unsigned short"]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "ImDrawIdx": "unsigned int"
+    }
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = 1
+        tc.presets_prefix = f"{self.settings.os}_{self.settings.build_type}_{self.settings.arch}"
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.build()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "imgui")
+        self.cpp_info.includedirs = ["include", "include/dear-imgui"]
+        self.cpp_info.libs = conan.tools.files.collect_libs(self)
