6bb8a4de94a4ddc703807878ed54c2ac542873f6
ShuangLiu <liushuang.work@gmail.com>
2023-06-06 13:37:18 +0100

    fix testing

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 import conan.tools.files
 from conan.tools.cmake import CMake, CMakeToolchain
-
+import os
 
 class IMGUIConan(ConanFile):
     name = "imgui"
@@ -9,22 +9,28 @@ class IMGUIConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "enable_testing": [True, False],
         "ImDrawIdx": ["unsigned int", "unsigned short"]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "enable_testing": True,
         "ImDrawIdx": "unsigned int"
     }
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = 1
+        tc.variables["ENABLE_TESTING"] = self.options.enable_testing
         tc.presets_prefix = f"{self.settings.os}_{self.settings.build_type}_{self.settings.arch}"
+        if self.settings.os == "Emscripten" and self.options.enable_testing:
+            tc.preprocessor_definitions["_X86_"] = 1
         tc.generate()
 
     def export_sources(self):
         conan.tools.files.copy(self, "*", self.recipe_folder, self.export_sources_folder)
+        conan.tools.files.copy(self, "*", os.path.join(self.recipe_folder, "..", "imgui_test_engine"), os.path.join(self.export_sources_folder, "imgui_test_engine"))
 
     def build(self):
         cmake = CMake(self)
@@ -36,3 +42,4 @@ class IMGUIConan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", "imgui")
         self.cpp_info.includedirs = ["include", "include/dear-imgui"]
         self.cpp_info.libs = conan.tools.files.collect_libs(self)
+        self.cpp_info.defines = ["IMGUI_USER_CONFIG=\"im_user_config.h\""]
---
+++ im_user_config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define IMGUI_DEFINE_MATH_OPERATORS
+#define ImDrawIdx unsigned int
+#if ENABLE_TESTING
+#include "imgui_te_imconfig.h"
+#endif
