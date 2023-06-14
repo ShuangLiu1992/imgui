2b4868081cc62ca6f0b0c5fd95f807b867bb684d
ShuangLiu <liushuang.work@gmail.com>
2023-06-14 11:45:39 +0100

    IMGUI_TEST_ENGINE_ENABLE_STD_FUNCTION

--- im_user_config.h
+++ im_user_config.h
@@ -3,5 +3,6 @@
 #define ImDrawIdx unsigned int
 #if IMGUI_ENABLE_TESTING
 #define IMGUI_TEST_ENGINE_ENABLE_COROUTINE_STDTHREAD_IMPL 1
+#define IMGUI_TEST_ENGINE_ENABLE_STD_FUNCTION 1
 #include "imgui_te_imconfig.h"
 #endif
